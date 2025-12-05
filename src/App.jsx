c0fdbbbd6b1772eec2735ff0dd8a015e2ad446fa
bruna <brunamant@gmail.com>
2025-12-05 10:17:36 -0300

    folder image alteration and app modification

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,29 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import viteLogo from '/costs.svg';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <div className="App">
-        <p>Costs</p>
-      </div>
-    </>
+    <Route>
+      <ul>
+        <li>Home</li>
+        <li>Contato</li>
+      </ul>
+      <Switch>
+      <Route exact path='/'>
+        <Home />
+      </Route>
+      <Route exact path='/company'>
+        <Company />
+      </Route>
+      <Route exact path='/contact'>
+        <Contact />
+      </Route>
+      <Route exact path='/newproject'>
+        <NewProject />
+      </Route>
+
+      </Switch>
+      <p>Footer</p>
+    </Route>
   );
 }
 
