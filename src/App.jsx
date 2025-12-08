f84cd0f13c0ca73819d9de182831e1173ec5a305
bruna <brunamant@gmail.com>
2025-12-08 14:31:52 -0300

    new components for navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,29 @@
-import { useState } from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import viteLogo from '/costs.svg';
+import Home from './components/pages/Home';
+import Company from './components/pages/Company';
+import NewProject from './components/pages/NewProject';
+import Projects from './components/pages/Projects';
+import Contact from './components/pages/Contact';
+import Container from './components/layout/Container';
+import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
 
 function App() {
   return (
-    <Route>
-      <ul>
-        <li>Home</li>
-        <li>Contato</li>
-      </ul>
-      <Switch>
-      <Route exact path='/'>
-        <Home />
-      </Route>
-      <Route exact path='/company'>
-        <Company />
-      </Route>
-      <Route exact path='/contact'>
-        <Contact />
-      </Route>
-      <Route exact path='/newproject'>
-        <NewProject />
-      </Route>
-
-      </Switch>
-      <p>Footer</p>
-    </Route>
+    <Router>
+      <Navbar />
+      <Container customClass="min-height">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/company" element={<Company />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/newproject" element={<NewProject />} />
+        </Routes>
+      </Container>
+      <Footer />
+    </Router>
   );
 }
 
