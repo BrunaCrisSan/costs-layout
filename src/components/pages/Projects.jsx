80dd1e14113c51b37866229a54ec78e30f06227b
bruna <brunamant@gmail.com>
2025-12-16 16:32:11 -0300

    Retrieving projects from the database

--- src/components/pages/Projects.jsx
+++ src/components/pages/Projects.jsx
@@ -1,22 +1,49 @@
 import { useLocation } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 import Message from '../layout/Message';
 import Container from '../layout/Container';
+import LinkButton from '../layout/LinkButton';
+import ProjectCard from '../project/ProjectCard';
 import styles from './Projects.module.css';
 function Projects() {
+  const [projects, setProjects] = useState([]);
   const location = useLocation();
   let message = '';
   if (location.state) {
     message = location.state.message;
   }
+  useEffect(() => {
+    fetch('http://localhost:5000/projects', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((resp) => resp.json())
+      .then((data) => {
+        setProjects(data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
-    <div>
-      <div>
+    <div className={styles.project_container}>
+      <div className={styles.title_container}>
         <h1>Meus Projetos</h1>
-        <a href="">Novo Projeto</a>
+        <LinkButton to="/newproject" text="Criar Projeto" />
       </div>
       {message && <Message type={'Sucess'} msg={message} />}
       <Container customClass="start">
-        <p>Projetos...</p>
+        {projects.length > 0 &&
+          projects.map((project) => (
+            <ProjectCard
+              key={project.id}
+              id={project.id}
+              name={project.name}
+              budget={project.budget}
+              category={project.category}
+            />
+          ))}
       </Container>
     </div>
   );
---
+++ src/components/project/ProjectCard.jsx
@@ -0,0 +1,25 @@
+import styles from './ProjectCard.module.css';
+import { BsPencil, BsFillTrashFill } from 'react-icons/bs';
+function ProjectCard({ id, name, budget, category }) {
+  return (
+    <div className={styles.project_card}>
+      <h4>{name}</h4>
+
+      <p>
+        <span>Orçamento:</span> R${Number(budget).toLocaleString('pt-BR')}
+      </p>
+
+      <p className={styles.category_text}>
+        <span className={styles[category.name.toLowerCase()]}></span>
+        {category.name}
+      </p>
+
+      <div>
+        <p>Editar</p>
+        <p>Remover</p>
+      </div>
+    </div>
+  );
+}
+
+export default ProjectCard;
