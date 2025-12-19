4316b4996c231f99a700cb819b5bdbf6adf45af4
bruna <brunamant@gmail.com>
2025-12-19 14:24:59 -0300

    displaying project details

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Home from './components/pages/Home';
 import Company from './components/pages/Company';
 import NewProject from './components/pages/NewProject';
 import Projects from './components/pages/Projects';
+import Project from './components/pages/Project';
 import Contact from './components/pages/Contact';
 import Container from './components/layout/Container';
 import Navbar from './components/layout/Navbar';
@@ -20,6 +21,7 @@ function App() {
           <Route path="/company" element={<Company />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/newproject" element={<NewProject />} />
+          <Route path="/project/:id" element={<Project />} />
         </Routes>
       </Container>
       <Footer />
